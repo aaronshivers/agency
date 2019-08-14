8eb6edf21f0e5cbf30f261bbc1403a7679022031
aaronshivers <aaron@moonmail.org>
2019-08-13 22:35:05 -0500

    created teammember component

--- src/components/Team.js
+++ src/components/Team.js
@@ -1,9 +1,25 @@
 import React from 'react'
 import { Container, Row, Col } from 'react-bootstrap'
-import { FaTwitter, FaFacebookF, FaLinkedinIn } from 'react-icons/fa'
+import TeamMember from './TeamMember'
 import img1 from '../assets/images/about/1.jpg'
 import img2 from '../assets/images/about/2.jpg'
 import img3 from '../assets/images/about/3.jpg'
+
+const memberData = [
+  {
+    name: 'Kay Garland',
+    title: 'Lead Designer',
+    image: img1
+  }, {
+    name: 'Larry Parker',
+    title: 'Lead Marketer',
+    image: img2
+  }, {
+    name: 'Diana Pertersen',
+    title: 'Lead Developer',
+    image: img3
+  }
+]
 
 const Team = () => (
   <section className="bg-light page-section" id="team">
@@ -15,78 +31,11 @@ const Team = () => (
         </Col>
       </Row>
       <Row>
-        <Col sm={ 4 }>
-          <div className="team-member">
-            <img className="mx-auto rounded-circle" src={ img1 } alt="" />
-            <h4>Kay Garland</h4>
-            <p className="text-muted">Lead Designer</p>
-            <ul className="list-inline social-buttons">
-              <li className="list-inline-item">
-                <a href="#">
-                  <FaTwitter />
-                </a>
-              </li>
-              <li className="list-inline-item">
-                <a href="#">
-                  <FaFacebookF />
-                </a>
-              </li>
-              <li className="list-inline-item">
-                <a href="#">
-                  <FaLinkedinIn />
-                </a>
-              </li>
-            </ul>
-          </div>
-        </Col>
-        <Col sm={ 4 }>
-          <div className="team-member">
-            <img className="mx-auto rounded-circle" src={ img2 } alt="" />
-            <h4>Larry Parker</h4>
-            <p className="text-muted">Lead Marketer</p>
-            <ul className="list-inline social-buttons">
-              <li className="list-inline-item">
-                <a href="#">
-                  <FaTwitter />
-                </a>
-              </li>
-              <li className="list-inline-item">
-                <a href="#">
-                  <FaFacebookF />
-                </a>
-              </li>
-              <li className="list-inline-item">
-                <a href="#">
-                  <FaLinkedinIn />
-                </a>
-              </li>
-            </ul>
-          </div>
-        </Col>
-        <Col sm={ 4 }>
-          <div className="team-member">
-            <img className="mx-auto rounded-circle" src={ img3 } alt="" />
-            <h4>Diana Pertersen</h4>
-            <p className="text-muted">Lead Developer</p>
-            <ul className="list-inline social-buttons">
-              <li className="list-inline-item">
-                <a href="#">
-                  <FaTwitter />
-                </a>
-              </li>
-              <li className="list-inline-item">
-                <a href="#">
-                  <FaFacebookF />
-                </a>
-              </li>
-              <li className="list-inline-item">
-                <a href="#">
-                  <FaLinkedinIn />
-                </a>
-              </li>
-            </ul>
-          </div>
-        </Col>
+        {
+          memberData.map((member, i) => (
+            <TeamMember key={ i } { ...member } />
+          ))
+        }
       </Row>
       <Row>
         <Col lg={ 8 } className="mx-auto text-center">
