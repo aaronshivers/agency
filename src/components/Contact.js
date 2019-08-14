921092c5d56da4b4710210a254437e334750e0c3
aaronshivers <aaron@moonmail.org>
2019-08-13 22:51:06 -0500

    refactored contact component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,48 +1,48 @@
 import React from 'react'
-import { Container } from 'react-bootstrap'
+import { Container, Row, Col, Form, Button } from 'react-bootstrap'
 import SectionHeader from './SectionHeader'
 
 const Contact = () => (
   <section className="page-section" id="contact">
-    <div className="container">
+    <Container>
       <SectionHeader
         title='Contact Us'
         subtitle='Lorem ipsum dolor sit amet consectetur.'
       />
-      <div className="row">
-        <div className="col-lg-12">
-          <form id="contactForm" name="sentMessage" noValidate="novalidate">
-            <div className="row">
-              <div className="col-md-6">
-                <div className="form-group">
-                  <input className="form-control" id="name" type="text" placeholder="Your Name *" required="required" data-validation-required-message="Please enter your name." />
+      <Row>
+        <Col lg={ 12 }>
+          <Form id="contactForm" name="sentMessage" noValidate="novalidate">
+            <Row>
+              <Col md={ 6 }>
+                <Form.Group>
+                  <Form.Control id="name" type="text" placeholder="Your Name *" required data-validation-required-message="Please enter your name." />
                   <p className="help-block text-danger"></p>
-                </div>
-                <div className="form-group">
-                  <input className="form-control" id="email" type="email" placeholder="Your Email *" required="required" data-validation-required-message="Please enter your email address." />
+                </Form.Group>
+                <Form.Group>
+                  <Form.Control id="email" type="email" placeholder="Your Email *" required data-validation-required-message="Please enter your email address." />
                   <p className="help-block text-danger"></p>
-                </div>
-                <div className="form-group">
-                  <input className="form-control" id="phone" type="tel" placeholder="Your Phone *" required="required" data-validation-required-message="Please enter your phone number." />
+                </Form.Group>
+                <Form.Group>
+                  <Form.Control id="phone" type="tel" placeholder="Your Phone *" required data-validation-required-message="Please enter your phone number." />
                   <p className="help-block text-danger"></p>
-                </div>
-              </div>
-              <div className="col-md-6">
+                </Form.Group>
+              </Col>
+              <Col md={ 6 }>
                 <div className="form-group">
                   <textarea className="form-control" id="message" placeholder="Your Message *" required="required" data-validation-required-message="Please enter a message."></textarea>
                   <p className="help-block text-danger"></p>
                 </div>
-              </div>
+              </Col>
               <div className="clearfix"></div>
-              <div className="col-lg-12 text-center">
+              <Col lg={ 12 } className="text-center">
                 <div id="success"></div>
-                <button id="sendMessageButton" className="btn btn-primary btn-xl text-uppercase" type="submit">Send Message</button>
-              </div>
-            </div>
-          </form>
-        </div>
-      </div>
-    </div>
+                <Button id="sendMessageButton" type="submit" variant="primary" size="xl" className="text-uppercase">Send Message</Button>
+              </Col>
+            </Row>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
   </section>
 )
 
